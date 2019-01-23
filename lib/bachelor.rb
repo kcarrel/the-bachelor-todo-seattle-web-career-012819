86044c0055ef0a9c76c707787705b0f6dcba894d
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-23 03:52:34 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -37,7 +37,15 @@ def count_contestants_by_hometown(data, hometown)
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |season, details|
+    details.each do |contestant|
+      contestant.each do |value, key|
+        if key == hometown
+          occupation = contestant["occupation"]
+          return occupation
+        end
+      end
+    end
 end
 
 def get_average_age_for_season(data, season)
