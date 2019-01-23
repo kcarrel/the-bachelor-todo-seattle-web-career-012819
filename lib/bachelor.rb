2b8fa9688e1e34af6570d57db43688e5a47d3954
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-23 03:33:30 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,6 +2,7 @@ def get_first_name_of_season_winner(data, season)
   data[season].each do |contestant|
     contestant.each do |key, value|
       if value == "Winner"
+        name = contestant["name"]
 end
 
 def get_contestant_name(data, occupation)
