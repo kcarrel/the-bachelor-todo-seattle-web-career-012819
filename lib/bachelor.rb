7659ea04ecdf8cc85caddddb1313f2b44817e09f
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-23 03:35:50 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -10,7 +10,14 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data[occupation].each do |contestant|
+    contestant.each do |key,value|
+      if value == occupation
+        name = contestant["name"]
+        return name 
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
