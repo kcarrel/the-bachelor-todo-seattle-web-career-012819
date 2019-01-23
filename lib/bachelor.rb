88410ea176840372da2ff7c10c525ec9b42bcdc1
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-23 03:57:16 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
       details.each do |contestant|
         contestant.each do |key, value|
           if key = "age"
-            add = value
+            add = value.to_i 
             average = (age_total + add)/2 
           end
         end
